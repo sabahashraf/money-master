fc9131a22aa8d5d9ed795a0d7600f924f7e18628
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-02-16 17:23:34 -0800

    developing js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 const income = document.getElementById('income');
+const incomeAmount = parseFloat(income.value);
 const food = document .getElementById('food');
+const foodExpense = parseFloat(food.value);
+const rent = document.getElementById('rent');
+const rentExpense = parseFloat(rent.value);
+const clothes = document.getElementById('clothes');
+const clothesExpense = parseFloat(clothes.value);
+const save = document.getElementById('save');
+
+
+const totalExpense = document.getElementById('total-exp');
+const balance = document.getElementById('balance');
+const savingAmount= document.getElementById('savings');
+const remainingAmount = document.getElementById('rest');
+console.log(save);
